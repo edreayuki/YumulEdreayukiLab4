d9cfdab2488b05394e67c7fa5aee46bb3da31cf9
Yuki <edreayuki.yumul.iics@ust.edu.ph>
2018-10-02 00:07:00 +0800

    -

--- app/src/main/java/com/yumul/edreayuki/yumuledreayukilab4/MainActivity.java
+++ app/src/main/java/com/yumul/edreayuki/yumuledreayukilab4/MainActivity.java
@@ -3,18 +3,37 @@ package com.yumul.edreayuki.yumuledreayukilab4;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d( "4ITF" , "OnCreate has executed.");
+        Button btn1 = (Button) findViewById(R.id.btn1) ;
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Back button is clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Next button is clicked", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
@@ -46,9 +65,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d("4itf" , "OnDestroy has executed..");
     }
 
-    public void display(View v) {
-        Toast.makeText(this, "Button is clicked",Toast.LENGTH_LONG).show();
-        //Snackbar.make(v, "button is clicked", Snackbar.LENGTH_SHORT).show();
-    }
+
 
 }
